1bd4ff8367248df53816ca4527887d94bf23ed36
Rishika Mathur <130182192+rishikam23@users.noreply.github.com>
2024-11-12 01:22:13 +0530

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -9,7 +9,7 @@ vehicle_baltimore=NEI[NEI$SCC %in% vehicle_scc & NEI$fips=="24510", ]
 # Aggregating by year
 vehicle_em=aggregate(Emissions~year,data=vehicle_baltimore,sum)
 
-# Plot: Motor Vehicle Emissions in Baltimore City
+# Plot 5: Motor Vehicle Emissions in Baltimore City
 barplot(vehicle_em$Emissions,names.arg=vehicle_em$year,col="purple",xlab="Year",ylab="Motor Vehicle PM2.5 Emissions(tons)",main="Motor Vehicle Emissions in Baltimore City (1999–2008)")
 dev.copy(png,"plot5.png")
 dev.off()
